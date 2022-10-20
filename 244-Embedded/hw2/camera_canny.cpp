0cb80a1961cb965e789762c9c8df569c5fed7232
zane <zane.a.karl@gmail.com>
2022-10-20 11:23:14 -0700

    Change output file names

--- 244-Embedded/hw2/camera_canny.cpp
+++ 244-Embedded/hw2/camera_canny.cpp
@@ -119,7 +119,8 @@ int main(int argc, char **argv)
 			/****************************************************************************
 			 * Write out the edge image to a file.
 			 ****************************************************************************/
-			sprintf(outfilename, "camera_s_%3.2f_l_%3.2f_h_%3.2f.pgm", sigma, tlow, thigh);
+			//			sprintf(outfilename, "camera_s_%3.2f_l_%3.2f_h_%3.2f.pgm", sigma, tlow, thigh);
+			sprintf(outfilename, "frame%03d_%3.2f_l_%3.2f_h_%3.2f.pgm", i, sigma, tlow, thigh);
 			if(VERBOSE) printf("Writing the edge iname in the file %s.\n", outfilename);
 			if(write_pgm_image(outfilename, edge, rows, cols, NULL, 255) == 0){
 				fprintf(stderr, "Error writing the edge image, %s.\n", outfilename);
