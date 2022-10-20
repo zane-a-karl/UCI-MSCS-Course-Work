9c422c1f438a249f39ce185b8becf54fdd3c77e8
zane <zane.a.karl@gmail.com>
2022-10-20 10:51:15 -0700

    Update camera_canny to process variable number of images

--- 244-Embedded/hw2/camera_canny.cpp
+++ 244-Embedded/hw2/camera_canny.cpp
@@ -1,5 +1,5 @@
 /**
-*/
+ */
 #include <ctime>
 #include <iostream>
 #include <unistd.h>
@@ -18,118 +18,131 @@ using namespace cv;
 
 int main(int argc, char **argv)
 {
-   char* dirfilename;        /* Name of the output gradient direction image */
-   char outfilename[128];    /* Name of the output "edge" image */
-   char composedfname[128];  /* Name of the output "direction" image */
-   unsigned char *image;     /* The input image */
-   unsigned char *edge;      /* The output edge image */
-   int rows, cols;           /* The dimensions of the image. */
-   float sigma,              /* Standard deviation of the gaussian kernel. */
-	 tlow,               /* Fraction of the high threshold in hysteresis. */
-	 thigh;              /* High hysteresis threshold control. The actual
-			        threshold is the (100 * thigh) percentage point
-			        in the histogram of the magnitude of the
-			        gradient image that passes non-maximal
-			        suppression. */
+	char* dirfilename;        /* Name of the output gradient direction image */
+	char outfilename[128];    /* Name of the output "edge" image */
+	char composedfname[128];  /* Name of the output "direction" image */
+	unsigned char *image;     /* The input image */
+	unsigned char *edge;      /* The output edge image */
+	int rows, cols;           /* The dimensions of the image. */
+	float sigma,              /* Standard deviation of the gaussian kernel. */
+		tlow,               /* Fraction of the high threshold in hysteresis. */
+		thigh;              /* High hysteresis threshold control. The actual
+													 threshold is the (100 * thigh) percentage point
+													 in the histogram of the magnitude of the
+													 gradient image that passes non-maximal
+													 suppression. */
+	int n_imgs; /* Number of images to process */
 
-   /****************************************************************************
+	/****************************************************************************
    * Get the command line arguments.
    ****************************************************************************/
-   if(argc < 4){
-   fprintf(stderr,"\n<USAGE> %s sigma tlow thigh [writedirim]\n",argv[0]);
-      fprintf(stderr,"      sigma:      Standard deviation of the gaussian");
-      fprintf(stderr," blur kernel.\n");
-      fprintf(stderr,"      tlow:       Fraction (0.0-1.0) of the high ");
-      fprintf(stderr,"edge strength threshold.\n");
-      fprintf(stderr,"      thigh:      Fraction (0.0-1.0) of the distribution");
-      fprintf(stderr," of non-zero edge\n                  strengths for ");
-      fprintf(stderr,"hysteresis. The fraction is used to compute\n");
-      fprintf(stderr,"                  the high edge strength threshold.\n");
-      fprintf(stderr,"      writedirim: Optional argument to output ");
-      fprintf(stderr,"a floating point");
-      fprintf(stderr," direction image.\n\n");
-      exit(1);
-   }
+	if(argc < 4){
+		fprintf(stderr,"\n<USAGE> %s sigma tlow thigh n_imgs [writedirim]\n",argv[0]);
+		fprintf(stderr,"      sigma:      Standard deviation of the gaussian");
+		fprintf(stderr," blur kernel.\n");
+		fprintf(stderr,"      tlow:       Fraction (0.0-1.0) of the high ");
+		fprintf(stderr,"edge strength threshold.\n");
+		fprintf(stderr,"      thigh:      Fraction (0.0-1.0) of the distribution");
+		fprintf(stderr," of non-zero edge\n                  strengths for ");
+		fprintf(stderr,"hysteresis. The fraction is used to compute\n");
+		fprintf(stderr,"                  the high edge strength threshold.\n");
+		fprintf(stderr,"      n_imgs:     Positive Integer.\n");
+		fprintf(stderr,"      writedirim: Optional argument to output ");
+		fprintf(stderr,"a floating point");
+		fprintf(stderr," direction image.\n\n");
+		exit(1);
+	}
 
-   sigma = atof(argv[1]);
-   tlow = atof(argv[2]);
-   thigh = atof(argv[3]);
-   rows = HEIGHT;
-   cols = WIDTH;
+	sigma = atof(argv[1]);
+	tlow = atof(argv[2]);
+	thigh = atof(argv[3]);
+	n_imgs = atoi(argv[4]);
+	rows = HEIGHT;
+	cols = WIDTH;
 
-   if(argc == 5) dirfilename = (char *) "dummy";
-	 else dirfilename = NULL;
+	if(argc == 6) dirfilename = (char *) "dummy";
+	else dirfilename = NULL;
 
-   VideoCapture cap;
-   // open the default camera (/dev/video0)
-   // Check VideoCapture documentation for more details
-   if(!cap.open(0)){
-				cout<<"Failed to open /dev/video0"<<endl;
-        return 0;
-	 }
-	 cap.set(CAP_PROP_FRAME_WIDTH, WIDTH);
-   cap.set(CAP_PROP_FRAME_HEIGHT,HEIGHT);
+	VideoCapture cap;
+	// open the default camera (/dev/video0)
+	// Check VideoCapture documentation for more details
+	if(!cap.open(0)){
+		cout<<"Failed to open /dev/video0"<<endl;
+		return 0;
+	}
+	cap.set(CAP_PROP_FRAME_WIDTH, WIDTH);
+	cap.set(CAP_PROP_FRAME_HEIGHT,HEIGHT);
 
-	 Mat frame, grayframe;
+	//	Mat frame[], grayframe;
+	Mat *frame = new Mat[n_imgs];
+	Mat *grayframe = new Mat[n_imgs];
+	image = new unsigned char[n_imgs];
 
-   printf("[INFO] (On the pop-up window) Press ESC to start Canny edge detection...\n");
-	 for(;;)
-   {
-			cap >> frame;
-	 		if( frame.empty() ) break; // end of video stream
-	 		imshow("[RAW] this is you, smile! :)", frame);
+	printf("[INFO] (On the pop-up window) Press ESC to start Canny edge detection...\n");
+	for(int i = 0; i < n_imgs; ++i)
+		{
+			cap >> frame[i];
+	 		if( frame[i].empty() ) break; // end of video stream
+			imshow("[RAW] this is you, smile! :)", frame[i]);
 	 		if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
-   }
+		}
 
-   clock_t begin, mid, end;
-   double time_elapsed, time_capture, time_process;
+	clock_t begin, mid, end;
+	double time_elapsed, time_capture, time_process;
 
-   begin = clock();
-   //capture
-	 cap >> frame;
-	 mid = clock();
-	 cvtColor(frame, grayframe, COLOR_BGR2GRAY);
-	 image = grayframe.data;
+	begin = clock();
+	//capture
+	for(int i = 0; i < n_imgs; ++i)
+		{
+			cap >> frame[i];
+			mid = clock();
+			cvtColor(frame[i], grayframe[i], COLOR_BGR2GRAY);
+			image[i] = grayframe[i].data;
 
-   /****************************************************************************
-   * Perform the edge detection. All of the work takes place here.
-   ****************************************************************************/
-   if(VERBOSE) printf("Starting Canny edge detection.\n");
-   if(dirfilename != NULL){
-      sprintf(composedfname, "camera_s_%3.2f_l_%3.2f_h_%3.2f.fim",
-      sigma, tlow, thigh);
-      dirfilename = composedfname;
-   }
-   canny(image, rows, cols, sigma, tlow, thigh, &edge, dirfilename);
 
-   /****************************************************************************
-   * Write out the edge image to a file.
-   ****************************************************************************/
-   sprintf(outfilename, "camera_s_%3.2f_l_%3.2f_h_%3.2f.pgm", sigma, tlow, thigh);
-   if(VERBOSE) printf("Writing the edge iname in the file %s.\n", outfilename);
-   if(write_pgm_image(outfilename, edge, rows, cols, NULL, 255) == 0){
-      fprintf(stderr, "Error writing the edge image, %s.\n", outfilename);
-      exit(1);
-   }
-   end = clock();
-   time_elapsed = (double) (end - begin) / CLOCKS_PER_SEC;
-   time_capture = (double) (mid - begin) / CLOCKS_PER_SEC;
-   time_process = (double) (end - mid) / CLOCKS_PER_SEC;
+			/****************************************************************************
+			 * Perform the edge detection. All of the work takes place here.
+			 ****************************************************************************/
+			if(VERBOSE) printf("Starting Canny edge detection.\n");
+			if(dirfilename != NULL){
+				sprintf(composedfname, "camera_s_%3.2f_l_%3.2f_h_%3.2f.fim",
+								sigma, tlow, thigh);
+				dirfilename = composedfname;
+			}
+			canny(image[i], rows, cols, sigma, tlow, thigh, &edge, dirfilename);
 
-	 imshow("[GRAYSCALE] this is you, smile! :)", grayframe);
+			/****************************************************************************
+			 * Write out the edge image to a file.
+			 ****************************************************************************/
+			sprintf(outfilename, "camera_s_%3.2f_l_%3.2f_h_%3.2f.pgm", sigma, tlow, thigh);
+			if(VERBOSE) printf("Writing the edge iname in the file %s.\n", outfilename);
+			if(write_pgm_image(outfilename, edge, rows, cols, NULL, 255) == 0){
+				fprintf(stderr, "Error writing the edge image, %s.\n", outfilename);
+				exit(1);
+			}
 
-   printf("Elapsed time for capturing+processing one frame: %lf + %lf => %lf seconds\n", time_capture, time_process, time_elapsed);
-   printf("FPS: %01lf\n", NFRAME/time_elapsed);
+			end = clock();
+			time_elapsed = (double) (end - begin) / CLOCKS_PER_SEC;
+			time_capture = (double) (mid - begin) / CLOCKS_PER_SEC;
+			time_process = (double) (end - mid) / CLOCKS_PER_SEC;
 
-	 grayframe.data = edge;
-   printf("[INFO] (On the pop-up window) Press ESC to terminate the program...\n");
-	 for(;;){
-		 imshow("[EDGE] this is you, smile! :)", grayframe);
-		 if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
-	 }
+			imshow("[GRAYSCALE] this is you, smile! :)", grayframe);
 
-    //free resrources    
-//		grayframe.release();
-//    delete image;
-    return 0;
+			printf("Elapsed time for capturing+processing one frame: %lf + %lf => %lf seconds\n", time_capture, time_process, time_elapsed);
+			printf("FPS: %01lf\n", NFRAME/time_elapsed);
+
+			grayframe[i].data = edge;
+			printf("[INFO] (On the pop-up window) Press ESC to terminate the program...\n");
+			for(int i = 0; i < n_imgs; ++i){
+				imshow("[EDGE] this is you, smile! :)", grayframe[i]);
+				if( waitKey(10) == 27 ) break; // stop capturing by pressing ESC
+			}
+		}
+
+	//free resrources
+	delete[] frame;
+	delete[] grayframe;
+	//		grayframe.release();
+	//    delete image;
+	return 0;
 }
